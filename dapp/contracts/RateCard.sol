4cddd2a26347026d04792a03512286aea4473548
andygray <andy@whatsthescore.co.uk>
2015-12-02 11:43:44 +0000

    3

--- dapp/contracts/RateCard.sol
+++ dapp/contracts/RateCard.sol
@@ -2,44 +2,44 @@ import "std.sol";
 
 contract RateCard is abstract {
     
-    struct Rate {
-        uint rateInWei;
-        uint maxAmountInWei;
-        uint untilTimestamp;
-    }
+    // struct Rate {
+    //     uint rateInWei;
+    //     uint maxAmountInWei;
+    // }
     
-    mapping (uint => Rate) public rates;
+    mapping (uint => uint) public rates;
     
     event RateInit(address owner);
-    event RateAdded(uint countryCode, uint rateInWei, uint maxAmountInWei, uint untilTimestamp);
+    event RateAdded(uint countryCode, uint rateInWei);
     
     function RateCard() {
         RateInit(msg.sender);
     }
     
-    function addRate(uint countryCode, uint rateInWei, uint maxAmountInWei, uint untilTimestamp) {
-      rates[countryCode] = Rate({rateInWei: rateInWei, maxAmountInWei: maxAmountInWei, untilTimestamp: untilTimestamp}); 
-      RateAdded(countryCode, rateInWei, maxAmountInWei, untilTimestamp);
+    function addRate(uint countryCode, uint rateInWei) {
+    //   rates[countryCode] = Rate({rateInWei: rateInWei, maxAmountInWei: maxAmountInWei}); 
+      rates[countryCode] =  rateInWei;
+      RateAdded(countryCode, rateInWei);
     }
 }
 
 contract RateCardAlpha is RateCard, owned, mortal {
    function RateCardAlpha() { 
         //uk
-        addRate(44, 200000000000000000, 100000000000000000000, 1451043060);
+        addRate(44, 2);
         //usa
-        addRate(1, 500000000000000000, 100000000000000000000, 1451043060);
+        addRate(1, 5);
         //india
-        addRate(91, 700000000000000000, 100000000000000000000, 1451043060);
+        addRate(91, 7);
    }
 }
 contract RateCardBeta is RateCard, owned, mortal {
        function RateCardBeta() { 
         //uk
-        addRate(44, 100000000000000000, 100000000000000000000, 1451043060);
+        addRate(44, 1);
         //usa
-        addRate(1, 600000000000000000, 100000000000000000000, 1451043060);
+        addRate(1, 6);
         //india
-        addRate(91, 750000000000000000, 100000000000000000000, 1451043060);
+        addRate(91, 8);
    }
 }
--- dapp/contracts/RateEx.sol
+++ dapp/contracts/RateEx.sol
@@ -1,4 +1,5 @@
 import "std.sol";
+import "RateCard.sol";
 
 contract RateEx is owned, named("RateEx") {
     
@@ -9,23 +10,58 @@ contract RateEx is owned, named("RateEx") {
         uint telephoneNumber;
         uint amountInWei;
         uint callInSeconds;
+        uint costInWei;
+        uint refundInWei;
         bool completed;
+        uint quality;
     }
     
     address[] public rateCards;
     CallTx[] public calls;
     
+    mapping (uint => bool) public destinations;
+    
+    event RateExInit(address owner);
     event AddRateCard(address rateCardAddress);
+    event AddDestination(uint destination);
     
     function RateEx() {
         
+        RateExInit(msg.sender);
+        
+        // destinations we cover
+        addDestination(44);
+        addDestination(1);
+        addDestination(91);
+        
         // default rate card
-        rateCards.push(0xdf315f7485c3a86eb692487588735f224482abe3);
-        rateCards.push(0x06b179aabf198ced0f98c8ceca905a920a137ef4);
+        addRateCard(0xdf315f7485c3a86eb692487588735f224482abe3);
+        addRateCard(0x17956ba5f4291844bc25aedb27e69bc11b5bda39);
         
         // add a few mock calls
-        calls.push(CallTx(0x0ec96244d9efcf1711b7383644abbe0f31bc5fcc, 0xdf315f7485c3a86eb692487588735f224482abe3, 44, 7930534450, 5 * 200000000000000000, 242, true));
-        
+        calls.push(CallTx(
+            0x0ec96244d9efcf1711b7383644abbe0f31bc5fcc, 
+            0xdf315f7485c3a86eb692487588735f224482abe3, 
+            44, 
+            7930534450, 
+            300 * 2, 
+            242,
+            242 * 2, 
+            58 * 2, 
+            true, 
+            4));
+            
+        calls.push(CallTx(
+            0x0ec96244d9efcf1711b7383644abbe0f31bc5fcc, 
+            0xdf315f7485c3a86eb692487588735f224482abe3, 
+            1, 
+            2024561111, 
+            600 * 5, 
+            59,
+            59 * 5, 
+            541 * 2, 
+            true, 
+            4));
     }
     
     function addRateCard(address rateCardAddress) {
@@ -33,5 +69,50 @@ contract RateEx is owned, named("RateEx") {
         AddRateCard(rateCardAddress);
     }
     
-
+    function addDestination(uint countryCode) onlyowner {
+        destinations[countryCode] = true;
+        AddDestination(countryCode);
+    }
+    
+    function lowestRate(uint countryCode) constant returns (uint lowestRate) {
+        
+        // don't cover that destination
+        if (destinations[countryCode] == false) throw;
+        
+        uint lowest = 999999;
+        for (uint i = 0; i < rateCards.length; i++) {
+            RateCard rc = RateCard(rateCards[i]);
+            uint rate = rc.rates(countryCode);
+            if (rate > 0 && rate < lowest) {
+               lowest = rate; 
+            }
+        }
+        
+        // no rate on exchange
+        if (lowest == 999999) throw;
+        
+        return lowest; 
+    }
+    
+    function lowestRateCard(uint countryCode) constant returns (uint lowestRate, address lowestRateCard) {
+        
+        // don't cover that destination
+        if (destinations[countryCode] == false) throw;
+        
+        address lowestCard = 0x0;
+        uint lowest = 999999;
+        for (uint i = 0; i < rateCards.length; i++) {
+            RateCard rc = RateCard(rateCards[i]);
+            uint rate = rc.rates(countryCode);
+            if (rate > 0 && rate < lowest) {
+               lowest = rate; 
+               lowestCard = rateCards[i]; 
+            }
+        }
+        
+        // no rate on exchange
+        if (lowest == 999999) throw;
+        
+        return [lowest, lowestCard]; 
+    }
 }
