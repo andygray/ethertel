438f83e4ded894ce5b5415404dedab3d53c7b27e
Andy Gray <andyg@cakesolutions.net>
2015-12-03 14:31:26 +0000

    sb

--- app/js/constants/web3.const.js
+++ app/js/constants/web3.const.js
@@ -2,5 +2,5 @@
 
 mbApp.constants
     .constant('Web3Config', {
-        PROVIDER: 'http://ethertel.on.ether.camp' + ':8555/sandbox/' + '02d354368473f840c470e875d3a4a51d5631e3b6'
+        PROVIDER: 'http://ethertel.on.ether.camp' + ':8555/sandbox/' + '6422c592354904bcda517ed4a6f2bb140dafeee6'
     });
