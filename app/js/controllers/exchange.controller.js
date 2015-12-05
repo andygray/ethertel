3a23b77fbc2f2b46ac48e89dd2105dca6dffd04a
James Morgan <james.morgan.e@googlemail.com>
2015-12-05 12:21:42 +0000

    Adding get balance method

--- app/js/controllers/exchange.controller.js
+++ app/js/controllers/exchange.controller.js
@@ -16,6 +16,10 @@
             $log.debug('Number of rate cards', vm.rateCardCount);
         });
 
+        RateEx.getBalance().then(function (balance) {
+            console.log('balance', balance);
+        });
+
         RateEx.getAllRateCard().then(function (rateCards) {
             $log.debug('Found All RateCards', rateCards);
             vm.rateCards = rateCards;
